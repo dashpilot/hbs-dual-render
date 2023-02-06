1e7583b2804928387d1338282785d53757205065
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-06 23:16:03 +0000

    tweaks

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
 
-var partialPath = path.join(process.cwd(), "src", "home.hbs");
-var partial = fs.readFileSync(partialPath, "utf-8");
-Handlebars.registerPartial("home", partial);
-
 async function render() {
   const resp = await fetch(
     "https://api.eu-central-1.linodeobjects.com/test/data.json"
   );
   var data = await resp.json();
+
+  var partialPath = path.join(process.cwd(), "src", "home.hbs");
+  var main = fs.readFileSync(partialPath, "utf-8");
+  Handlebars.registerPartial("main", main);
 
   var file = path.join(process.cwd(), "src", "layout.hbs");
   var source = fs.readFileSync(file, "utf-8");
@@ -21,5 +21,8 @@ async function render() {
   fs.mkdirSync("./public");
 
   fs.writeFileSync("./public/index.html", result, "utf-8");
+
+  fs.writeFileSync("./public/_src/layout.hbs", source, "utf-8");
+  fs.writeFileSync("./public/_src/main.hbs", main, "utf-8");
 }
 render();
