b1d737482f232d46db90c31e538baa7bc5df5fca
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-28 21:33:04 +0000

    test

--- api/render.js
+++ api/render.js
@@ -1,18 +1,7 @@
-const Handlebars = require("handlebars");
-const fs = require("fs");
-const path = require("path");
+const { render } = require("./render");
 
 module.exports = async (req, res) => {
-  const resp = await fetch(
-    "https://api.eu-central-1.linodeobjects.com/test/data.json"
-  );
-  var data = await resp.json();
-
-  var file = path.join(process.cwd(), "src", "main.hbs");
-  var source = fs.readFileSync(file, "utf-8");
-
-  var template = Handlebars.compile(source);
-  var result = template(data);
+  const result = await render();
 
   res.send(result);
 };
