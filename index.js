7ef52e792cefb46187e2da90d413f757ba7fa291
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-28 21:56:53 +0000

    generate pages

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const fs = require("fs");
 const { hbs_render } = require("./api/hbs_render");
 
 async function prerender() {
-  const result = await hbs_render("home");
-
   fs.mkdirSync("./public");
   fs.mkdirSync("./public/tpl");
 
+  var result = await hbs_render("home");
   fs.writeFileSync("./public/index.html", result, "utf-8");
+
+  var result = await hbs_render("about");
+  fs.mkdirSync("./public/about");
+  fs.writeFileSync("./public/about/index.html", result, "utf-8");
+
+  var result = await hbs_render("contact");
+  fs.mkdirSync("./public/contact");
+  fs.writeFileSync("./public/contact/index.html", result, "utf-8");
 
   // fs.writeFileSync("./public/tpl/layout.html", source, "utf-8");
   // fs.writeFileSync("./public/tpl/main.html", main, "utf-8");
