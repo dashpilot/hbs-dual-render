9723e658955b0872601200f03bd168c714c19de1
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-28 21:36:44 +0000

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { hbs_render } = require("./render");
+const { hbs_render } = require("./hbs_render");
 
 async function prerender() {
   const result = await hbs_render();
