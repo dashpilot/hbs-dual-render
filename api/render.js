c2f6501e765e1849ab3e7925b22d01f2f18b5b72
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-03-29 12:06:06 +0000

    Created 403 page

--- api/render.js
+++ api/render.js
@@ -10,6 +10,19 @@ module.exports = async (req, res) => {
 
     res.send(result);
   } else {
-    res.status(403).send(`<h1>403 Forbidden</h1>`);
+    res.status(403).send(`<!doctype html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" />
+        <title>Error 403 - Forbidden</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+      </head
+      <body>
+        <div class="container mt-5 text-center">
+        <h1>403 Forbidden</h1>
+        </div>
+      </body>
+      </html>`);
   }
 };
